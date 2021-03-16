ff034e86493fa653a56a9fbe33df52e3a4f35c7f
CuentaBorrada <amateo1982@yahoo.es>
2021-03-16 21:46:10 +0100

    Inicializando estilos principales de la app

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola Paola que tal??</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.droidSafeArea}>
+      <View>
+        <Text>Hola Paola que tal??</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  droidSafeArea: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#ff0",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
   },
 });
