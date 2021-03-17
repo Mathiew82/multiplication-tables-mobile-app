d49c5b8c193f4d30b6a1a3f3c712dc5f7e677dcc
CuentaBorrada <amateo1982@yahoo.es>
2021-03-17 23:51:30 +0100

    Definiendo estilos principales

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.droidSafeArea}>
       <View>
-        <Text>Hola Paola que tal??</Text>
+        <Text style={styles.textHeader}>Multiplication Tables</Text>
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
@@ -16,7 +16,14 @@ export default function App() {
 const styles = StyleSheet.create({
   droidSafeArea: {
     flex: 1,
-    backgroundColor: "#ff0",
-    paddingTop: Platform.OS === "android" ? 25 : 0,
+    backgroundColor: "#1d31a7",
+    paddingTop: Platform.OS === "android" ? 60 : 0,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  textHeader: {
+    color: "#fff",
+    fontSize: 30,
+    textAlign: "center",
   },
 });
