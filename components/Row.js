ad88f8b199d6196f079f2f4043c77b90f17a5d32
CuentaBorrada <amateo1982@yahoo.es>
2021-03-22 22:29:15 +0100

    Agregado el input y estilos pasados a StyleSheet

--- components/Row.js
+++ components/Row.js
@@ -1,66 +1,57 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function Row(props) {
   const { firstValue, secondValue } = props;
+  const [text, onChangeText] = React.useState("");
 
   return (
-    <View
-      style={{
-        borderBottomWidth: 2,
-        borderBottomColor: "#000",
-        flexDirection: "row",
-      }}
-    >
+    <View style={styles.tr}>
       <View
         style={{
-          width: "25%",
+          width: "20%",
           border: "none",
-          paddingTop: 15,
-          paddingRight: 10,
-          paddingBottom: 15,
-          paddingLeft: 10,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          ...styles.td,
         }}
       >
-        <Text
-          style={{
-            color: "#000",
-            fontSize: 18,
-            fontWeight: "bold",
-            textAlign: "center",
-          }}
-        >
+        <Text style={styles.text}>
           {firstValue} x {secondValue + 1}
         </Text>
       </View>
       <View
         style={{
-          width: "50%",
+          width: "60%",
           borderLeftWidth: 2,
           borderLeftColor: "#000",
-          paddingTop: 15,
-          paddingRight: 10,
-          paddingBottom: 15,
-          paddingLeft: 10,
+          ...styles.td,
         }}
-      ></View>
+      >
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          placeholder="Introduce un valor"
+          value={text}
+        />
+      </View>
       <View
         style={{
-          width: "25%",
+          width: "20%",
           borderLeftWidth: 2,
           borderLeftColor: "#000",
-          paddingTop: 15,
-          paddingRight: 10,
-          paddingBottom: 15,
-          paddingLeft: 10,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          ...styles.td,
         }}
       >
         <Text
           style={{
-            color: "#000",
-            fontSize: 18,
-            fontWeight: "bold",
-            textAlign: "center",
+            ...styles.text,
             transform: [{ rotate: "90deg" }],
           }}
         >
@@ -70,3 +61,32 @@ export default function Row(props) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  tr: {
+    borderBottomWidth: 2,
+    borderBottomColor: "#000",
+    flexDirection: "row",
+  },
+  td: {
+    paddingTop: 5,
+    paddingRight: 10,
+    paddingBottom: 5,
+    paddingLeft: 10,
+  },
+  input: {
+    width: "65%",
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    margin: 0,
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+  text: {
+    color: "#000",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
